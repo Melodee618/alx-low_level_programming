450c8db33d2ba8a87168b9f2b4e96706729ac18d
Melodee618 <melodeeokigbo@gmail.com>
2022-03-13 19:58:28 +0100

    Print alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char a = 97
+
+	while (a <= 122 && != 101 && != 113)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10);
+	return (0);
+}
