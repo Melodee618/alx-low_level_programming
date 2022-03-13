e7c620854a29b463d516b8627a833bc45aac8d3b
Melodee618 <melodeeokigbo@gmail.com>
2022-03-13 20:49:47 +0100

    Print digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i = '1';
+
+	while (i < '10')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
