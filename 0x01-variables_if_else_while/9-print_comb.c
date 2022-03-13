e3486aa6308459f4bf3ae210984b5f32bbe6db94
Melodee618 <melodeeokigbo@gmail.com>
2022-03-13 22:09:39 +0100

    Number combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(320;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
