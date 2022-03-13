c900ad44f14122a41589fe99f3ffaf462a93635b
Melodee618 <melodeeokigbo@gmail.com>
2022-03-13 19:25:24 +0100

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char a = 97;
+
+	while( a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar(10)
+}
