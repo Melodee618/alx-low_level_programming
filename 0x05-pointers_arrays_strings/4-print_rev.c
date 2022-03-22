1a413ff208edb34a6cc019bfc223c4c816674ced
Melodee618 <melodeeokigbo@gmail.com>
2022-03-22 22:00:50 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_rev - Prints a string in reverse
+* @s: pointer
+*
+* Return: nothing
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
