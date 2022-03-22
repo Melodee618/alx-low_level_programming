db25852f05e5a9e362fa230e704ac4dd4f72508c
Melodee618 <melodeeokigbo@gmail.com>
2022-03-22 00:38:24 +0000

    Fixed styling err

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 /**
 * reset_to_98 - reset n value to 98
-* @*n - pointer storing n address
+* @n: pointer storing address
 *
-* Description: Resetting the value of n to 98 using the variable address.
-*
+* Description: Resetting the value of the variable stored at *n address to 98
+* using a pointer.
 * Return: nothing.
 */
 void reset_to_98(int *n)
