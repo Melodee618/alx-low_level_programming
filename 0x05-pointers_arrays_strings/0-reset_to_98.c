b22536dc2624a04d74a6e441dea6f6ac5915ac66
Melodee618 <melodeeokigbo@gmail.com>
2022-03-22 00:28:17 +0000

    First commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+* reset_to_98 - reset n value to 98
+* @*n - pointer storing n address
+*
+* Description: Resetting the value of n to 98 using the variable address.
+*
+* Return: nothing.
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
