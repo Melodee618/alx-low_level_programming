7dc4e47abc15d6b170a9a1c52c4755a271c43cba
Melodee618 <melodeeokigbo@gmail.com>
2022-04-12 20:50:47 +0000

    First commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Using typedef and structs in the C programming language for the first time!
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+* struct dog - a structure with three elements char *, float, char *
+* @name:stores dog's name when init
+* @age: stores dog's age when init
+* @owner: stores owner of dog when init
+*
+* Description: a struct that stores info about a dog when inti.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
