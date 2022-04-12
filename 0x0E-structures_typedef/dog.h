7abe7e0f1e4c1d6ec7ecb3da2b94c7fe8b125144
Melodee618 <melodeeokigbo@gmail.com>
2022-04-12 22:03:22 +0000

    Added typedef for struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,12 @@
 #define _DOG_H_
 
 /**
+* dog_t - typedef for struct dog
+*/
+
+typedef struct dog dog_t;
+
+/**
 * struct dog - a structure with three elements char *, float, char *
 * @name:stores dog's name when init
 * @age: stores dog's age when init
